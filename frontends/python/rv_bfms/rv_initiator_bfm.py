914ef3c143957e47f0d11e2ad47de71e06ce017c
Matthew Ballance <matt.ballance@gmail.com>
2021-12-22 12:50:39 -0800

    Tweaks during Verilator and tblink-rpc-cocotb bring-up
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- frontends/python/rv_bfms/rv_initiator_bfm.py
+++ frontends/python/rv_bfms/rv_initiator_bfm.py
@@ -3,32 +3,34 @@ Created on Nov 13, 2021
 
 @author: mballance
 '''
-import tblink
+import tblink_rpc
 import ctypes
 
-@tblink.iftype("rv_bfms.initiator")
+@tblink_rpc.iftype("rv_bfms.initiator")
 class RvInitiatorBfm(object):
     
     def __init__(self):
-        self.ev = tblink.Event()
+        self.ev = tblink_rpc.Event()
         
     async def send(self, data):
         self.ev.clear()
+        print("--> self.req", flush=True)
         await self.req(data)
+        print("<-- self.req", flush=True)
         
         if not self.ev.is_set():
-            print("--> await")
+            print("--> await", flush=True)
             await self.ev.wait()
-            print("<-- await")
+            print("<-- await", flush=True)
 
-    @tblink.exptask
+    @tblink_rpc.exptask
     async def req(self, data : ctypes.c_uint64):
         pass
     
-    @tblink.imptask
-    async def rsp(self):
-        print("--> rsp")
+    @tblink_rpc.impfunc
+    def rsp(self):
+        print("--> rsp", flush=True)
         self.ev.set()
-        print("<-- rsp")
+        print("<-- rsp", flush=True)
     
     pass
